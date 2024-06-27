a0ddb34ddd4c2a188f3c1fbcc8146af420a5bddb
TEJWARDEEP SINGH <tejwardeep@gmail.com>
2024-06-27 22:19:16 +0530

    updates(27/06/2024)

--- script.js
+++ script.js
@@ -81,6 +81,22 @@ function nav(){
 
 }
 nav()
+function page1(){
+    var tl=gsap.timeline()
+    tl.from("#page1 #text",{
+        x:-1000,
+        y:-500,
+        duration:1,
+        opacity:0
+    })
+    tl.from("#page1 #pg1-image-container",{
+        x:1000,
+        y:500,
+        duration:1,
+        opacity:0
+    })
+}
+page1()
 function page2(){
     gsap.from(".category",{
         opacity:0,
@@ -89,11 +105,23 @@ function page2(){
         scrollTrigger:{
             trigger:"#page2 h1",
             scroller:"body",
-            start:"top 50%"
         }
     })
 }
 page2()
+function page3(){
+    gsap.from("#watch-container img,#watch-queue1",{
+        scale:0,
+        opacity:0,
+        duration:2,
+        stagger:0.5,
+        scrollTrigger:{
+            trigger:"#watch-container img ,watch-queue1",
+            scroller:"body",
+        }
+    })
+}
+page3()
 function page4(){
     var tl=gsap.timeline();
     tl.from("#pg4-container h1",{
@@ -117,24 +145,29 @@ function page4(){
         }
     })
 }
-function page3(){
-    gsap.from("#watch-container img",{
-        scale:0,
+page4()
+function page5(){
+    gsap.from("#pg5-container1 .slide",{
         opacity:0,
-        duration:2,
-        delay:0.5,
+        duration:1,
+        stagger:0.2,
         scrollTrigger:{
-            trigger:"#watch-container img",
-            scroller:"body"
+            trigger:"#pg5-container1 .slide",
+            scroller:"body",
         }
     })
 }
-page3()
-page4()
-function page5(){
-}
 page5()
 function page6(){
+    gsap.from("#page6 #video",{
+        opacity:0,
+        duration:1,
+        scale:"0",
+        scrollTrigger:{
+            trigger:"#page6 #video",
+            scroller:"body"
+        }
+    })
     var vid=document.querySelector("#page6 video");
     vid.addEventListener("mouseenter",function(){
         vid.play();
@@ -144,27 +177,72 @@ function page6(){
     })
 }
 page6()
-function footer(){
-    var tl=gsap.timeline();
-    tl.from(".fl,.fc,.fr",{
+function page7(){
+    gsap.from("#pg7-container .trending",{
         opacity:0,
-        duration:2,
-        delay:0.1,
-        stagger:0.4,
+        duration:1,
+        stagger:0.2,
         scrollTrigger:{
-            trigger:".fl,.fc,.fr",
-            scroller:"body"
+            trigger:"#pg7-container .trending",
+            scroller:"body",
         }
     })
-    tl.from("#horizontal-ruler,#footer-p h1,#footer-p h5",{
+}
+page7()
+function page9(){
+    gsap.from("#pg9-container a",{
         opacity:0,
-        duration:2,
-        delay:0.1,
-        stagger:0.4,
+        duration:1,
+        stagger:0.2,
         scrollTrigger:{
-            trigger:".fl,.fc,.fr",
-            scroller:"body"
+            trigger:"#pg9-container a",
+            scroller:"body",
+        }
+    })
+}
+page9()
+function footer(){
+    gsap.from("#footer .fl,#footer .fc,#footer .fr",{
+        opacity:0,
+        duration:1,
+        stagger:0.2,
+        scrollTrigger:{
+            trigger:"#footer .fl,#footer .fc,#footer .fr",
+            scroller:"body",
         }
     })
 }
 footer()
+function footer_p(){
+    var tl2=gsap.timeline();
+    gsap.from("#horizontal-ruler",{
+        opacity:0,
+        duration:0.5,
+        delay:0.5,
+        scrollTrigger:{
+            trigger:"#horizontal-ruler",
+            scroller:"body"
+        }
+    })
+    tl2.from("#footer-p h1",{
+        x:-1000,
+        opacity:0,
+        duration:0.5,
+        delay:0.5,
+        scrollTrigger:{
+            trigger:"#footer-p h1",
+            scroller:"body"
+        }
+    })
+    tl2.from("#footer-p h5",{
+        x:1000, 
+        opacity:0,
+        duration:0.5,
+        delay:0.5,
+        scrollTrigger:{
+            trigger:"#footer-p h5",
+            scroller:"body"
+        }
+    })
+}
+footer_p()
