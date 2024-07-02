9ed4d69c2dee218b77e656cc117866f946d31173
TEJWARDEEP SINGH <tejwardeep@gmail.com>
2024-07-02 21:07:42 +0530

    updates(2/07/2024)

--- script.js
+++ script.js
@@ -118,18 +118,15 @@ function nav(){
     })
 
     var box2 = document.querySelector("#menu")
+    var box3= document.querySelector("#nr")
 
-    box2.addEventListener("click",function(){
-        gsap.to("#nr",{
-            display:"flex"
-        })
-    })
-    box2.addEventListener("mouseleave",function(){
-        gsap.to("#nr",{
-            display:"none",
-            delay:2
-        })
-    })
+    box2.addEventListener('click', function() {
+        if (box3.style.display === 'none') {
+            box3.style.display = 'flex';
+        } else {
+            box3.style.display = 'none';
+        }
+    });
 
 }
 nav()
